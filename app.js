743af7027287bc95180879a40fa18dff294282d8
Jonathan Natividad <pnatividad027@gmail.com>
2021-06-23 21:05:01 +0800

    second commit - updated

--- app.js
+++ app.js
@@ -158,7 +158,12 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-// -------------------------------
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+
+// Listening to server
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port, function(){
+  console.log("Server has started");
 });
